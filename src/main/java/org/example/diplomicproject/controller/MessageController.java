01e1762a58ee1b50883b1c43201958145c43d0b2
PavelUkhov <164402643+PaulUkhov@users.noreply.github.com>
2025-07-03 18:59:06 +0300

    ThirdCommit

---
+++ src/main/java/org/example/diplomicproject/controller/MessageController.java
@@ -0,0 +1,24 @@
+package org.example.diplomicproject.controller;
+
+//import org.example.diplomicproject.service.MessageSender;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/send")
+//public class MessageController {
+//
+//    private final MessageSender sender;
+//
+//    public MessageController(MessageSender sender) {
+//        this.sender = sender;
+//    }
+//
+//    @PostMapping
+//    public void sendMessage(@RequestBody String message) {
+//        sender.send(message);
+//    }
+//}
+
